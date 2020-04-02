dfc3675c92a9a835643ae7dc548f06b0a4c8be4e
alvinator <a.cirillo@bitdrome.com>
2020-04-02 12:08:17 +0200

    Updates

---
+++ precache-manifest.f72c6912e6b09b18fecd1f7661952b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e103a364e61d3922f278de892f68c30",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "760cde3a102ffdd2e0f9",
+    "url": "/monster-rolodex/static/css/main.becd6cf6.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760cde3a102ffdd2e0f9",
+    "url": "/monster-rolodex/static/js/main.adf8904d.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
